a4ef76bc2c9f6b528cef07c6a6dfbd2aa6867d4e
vongockhoi <vongockhoi1996@gmail.com>
2021-04-10 01:37:57 -0700

    Create bai29.py

---
+++ python/testPython/bai29.py
@@ -0,0 +1,9 @@
+try:
+    a = int(input())
+    dem = 1
+    for so in range(1,a+1):
+        for so1 in range(1,so+1):
+            print(so1, end=" ")
+        print('')
+except:
+    print("loi dinh dang")
