98a376371e1926b604c66f3681c04cf6382bcb16
vongockhoi <vongockhoi1996@gmail.com>
2021-04-10 01:38:00 -0700

    Create bai30.py

---
+++ python/testPython/bai30.py
@@ -0,0 +1,9 @@
+try:
+    a = int(input())
+    dem = 1
+    for so in range(a,0,-1):
+        for so1 in range(so,0,-1):
+            print(so1,end=" ")
+        print("")
+except:
+    print("loi dinh dang")
