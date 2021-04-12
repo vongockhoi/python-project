81ddf9f1428a21e39217f0b0a9bb109043d9c9ae
vongockhoi <vongockhoi1996@gmail.com>
2021-04-12 22:27:12 +0700

    Create bai31.py

---
+++ python/testPython/bai31.py
@@ -0,0 +1,28 @@
+import string
+lista  = list(string.ascii_lowercase)
+print(lista)
+listcong = []
+i = 1
+tongkytu = 0
+nhotam = 0
+try:
+    a = int(input())
+    
+    for lis in range(0,a):
+        listcong.append(i)
+        tongkytu +=i
+        i +=2
+    for lis in range(0,a):
+        for lis1 in range(0,listcong[a-1]):
+            print(" ",end=" ")
+            if lis1 == a-(lis+1):
+                for inkytu in range(0,listcong[lis]):
+                    print(lista[nhotam], end=" ")
+                    nhotam +=1 
+                    if nhotam == 26:
+                        nhotam = 0
+        print("")
+
+    print(listcong[a-1], len(lista))
+except:
+    print("nhập sai phải nhập số tự nhiên")
