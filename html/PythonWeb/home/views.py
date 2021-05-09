203e31c40d5fa453355cbb83695ce0ad4cf0cd58
vongockhoi <vongockhoi1996@gmail.com>
2021-05-08 18:31:08 -0700

    7.5

--- html/PythonWeb/home/views.py
+++ html/PythonWeb/home/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    response = HttpResponse()
-    response.writelines("<h1>xin chao</h1>")
-    response.write("day la apphome")
-    return response    
+    return render(request,'pages/home.html')    
